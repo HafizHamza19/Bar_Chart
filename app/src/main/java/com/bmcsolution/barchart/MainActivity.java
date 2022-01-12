ac6b706ec18d1a200485ca048ccc6e6668d2bfdf
hafizhamza19 <hafizhamza0322@gmail.com>
2022-01-12 15:09:49 +0500

    first commit

---
+++ app/src/main/java/com/bmcsolution/barchart/MainActivity.java
@@ -0,0 +1,103 @@
+package com.bmcsolution.barchart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.interfaces.datasets.IDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    final String[] months = new String[] {
+            "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    };
+    public static final int[] VORDIPLOM_COLORS = {
+            Color.rgb(192, 255, 140), Color.rgb(255, 247, 140), Color.rgb(255, 208, 140),
+            Color.rgb(140, 234, 255), Color.rgb(255, 140, 157),Color.rgb(207, 248, 246), Color.rgb(148, 212, 212), Color.rgb(136, 180, 187),
+            Color.rgb(118, 174, 175), Color.rgb(42, 109, 130),Color.rgb(118, 174, 200), Color.rgb(42, 109, 150)
+    };
+    private BarChart chart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        chart = findViewById(R.id.chart1);
+
+        chart.getDescription().setEnabled(false);
+
+        // if more than 60 entries are displayed in the chart, no values will be
+        // drawn
+        chart.setMaxVisibleValueCount(60);
+
+        // scaling can now only be done on x- and y-axis separately
+        chart.setPinchZoom(false);
+
+
+        chart.setDrawBarShadow(false);
+        chart.setDrawGridBackground(false);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setLabelCount(12);
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return months[(int) value % months.length];
+            }
+
+        });
+        chart.getAxisLeft().setDrawGridLines(false);
+
+
+
+
+        // add a nice and smooth animation
+        chart.animateY(1500);
+
+        chart.getLegend().setEnabled(false);
+
+
+
+        ArrayList<BarEntry> values = new ArrayList<>();
+        // setting data
+        //add entry here
+        for (int i = 0; i < 12; i++) {
+            values.add(new BarEntry(i, i+2*10));
+        }
+
+        BarDataSet set1;
+
+        if (chart.getData() != null && chart.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet) chart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            chart.getData().notifyDataChanged();
+            chart.notifyDataSetChanged();
+        } else {
+        set1 = new BarDataSet(values, "Data Set");
+            set1.setColors(/*ColorTemplate.VORDIPLOM_COLORS*/VORDIPLOM_COLORS);
+            set1.setDrawValues(false);
+
+            ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+            dataSets.add(set1);
+
+            BarData data = new BarData(dataSets);
+            chart.setData(data);
+            chart.setFitBars(true);
+        }
+  for (IDataSet set : chart.getData().getDataSets())
+            set.setDrawValues(!set.isDrawValuesEnabled());
+        chart.invalidate();
+    }
+}
